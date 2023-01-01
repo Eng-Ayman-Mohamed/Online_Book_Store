237f3c51ced2714904637f93aff5cead123875a9
Ahmed Ezz <95645898+A7med3z@users.noreply.github.com>
2023-01-01 16:44:50 +0200

    Revert "spring initializer"
    This reverts commit 1de7055f0ee877a99bcc37c1b1133ecf27bf757d.

--- Backend/src/main/java/com/example/demo/BookStore/BookStoreController.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.BookStore;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class BookStoreController {
-
-}
