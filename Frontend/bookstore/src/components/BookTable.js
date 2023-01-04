a9418514cf02b2d0229f0f97d66e878cbb59b86a
AbdelRahman Adel AbdelFatttah <56457765+ad3ldev@users.noreply.github.com>
2023-01-04 15:56:43 +0200

    book table done

--- Frontend/bookstore/src/components/BookTable.js
+++ Frontend/bookstore/src/components/BookTable.js
@@ -1,4 +1,5 @@
 import "./BookTable.css";
+import { useState } from "react";
 const BookTable = () => {
 	let books = [
 		{
@@ -56,27 +57,83 @@ const BookTable = () => {
 			publisher: "Jump",
 		},
 	];
+	const [page, setPage] = useState(1);
+	let content = [];
+	let size = 2;
+	let start = (page - 1) * size;
+	let end = page * size;
+
+	for (let i = start; i < end; i++) {
+		content.push(
+			<a className='tr' href='/' key={books[i].ISBN}>
+				<div className='td'>{books[i].ISBN}</div>
+				<div className='td'>{books[i].title}</div>
+				<div className='td'>{books[i].year}</div>
+				<div className='td'>{books[i].category}</div>
+				<div className='td'>{books[i].price}</div>
+				<div className='td'>{books[i].amount}</div>
+				<div className='td'>{books[i].publisher}</div>
+			</a>,
+		);
+	}
+	function changePage() {
+		content = [];
+		let start = (page - 1) * size;
+		let end = page * size;
+		for (let i = start; i < end; i++) {
+			content.push(
+				<a className='tr' href='/' key={books[i].ISBN}>
+					<div className='td'>{books[i].ISBN}</div>
+					<div className='td'>{books[i].title}</div>
+					<div className='td'>{books[i].year}</div>
+					<div className='td'>{books[i].category}</div>
+					<div className='td'>{books[i].price}</div>
+					<div className='td'>{books[i].amount}</div>
+					<div className='td'>{books[i].publisher}</div>
+				</a>,
+			);
+		}
+	}
+
+	let notFound = books == null ? true : false;
+
 	return (
-		<div className='table'>
-			<div className='th'>ISBN</div>
-			<div className='th'>Title</div>
-			<div className='th'>Year</div>
-			<div className='th'>Category</div>
-			<div className='th'>Price</div>
-			<div className='th'>Amount</div>
-			<div className='th'>Publisher</div>
-			{books.map((book) => (
-				<a className='tr' href='/' key={book.ISBN}>
-					<div className='td'>{book.ISBN}</div>
-					<div className='td'>{book.title}</div>
-					<div className='td'>{book.year}</div>
-					<div className='td'>{book.category}</div>
-					<div className='td'>{book.price}</div>
-					<div className='td'>{book.amount}</div>
-					<div className='td'>{book.publisher}</div>
-				</a>
-			))}
-		</div>
+		<>
+			{!notFound && (
+				<div className='table'>
+					<div className='th'>ISBN</div>
+					<div className='th'>Title</div>
+					<div className='th'>Year</div>
+					<div className='th'>Category</div>
+					<div className='th'>Price</div>
+					<div className='th'>Amount</div>
+					<div className='th'>Publisher</div>
+					{content.map((item) => item)}
+				</div>
+			)}
+			<div className='changing-pages'>
+				{page !== 1 && (
+					<button
+						onClick={() => {
+							setPage(page - 1);
+							changePage();
+						}}>
+						Previous
+					</button>
+				)}
+				<p>{page}</p>
+				{page * size < books.length && (
+					<button
+						onClick={() => {
+							setPage(page + 1);
+							changePage();
+						}}>
+						Next
+					</button>
+				)}
+			</div>
+			{notFound && <p>No results</p>}
+		</>
 	);
 };
 
