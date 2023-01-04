ab7e002eb2dfd9d6f6591af79dafdc3695ccf0c3
AbdelRahman Adel AbdelFatttah <56457765+ad3ldev@users.noreply.github.com>
2023-01-04 23:29:53 +0200

    Header works

--- Frontend/bookstore/src/CustomerPage/Customerpage.js
+++ Frontend/bookstore/src/CustomerPage/Customerpage.js
@@ -1,113 +1,70 @@
-import React from 'react';
-import './Customerpage.css';
-import BookCard from './components/BookCard';
-import Header from './components/Header';
-import { BsFillCartCheckFill } from 'react-icons/bs';
-import { useNavigate, useLocation } from 'react-router-dom';
-import {BsBoxArrowLeft , BsFillPersonFill, BsFillFilterCircleFill } from 'react-icons/bs';
-import {environment} from '../Environment';
+import React from "react";
+import "./Customerpage.css";
+import BookCard from "./components/BookCard";
+import { BsFillCartCheckFill } from "react-icons/bs";
+import { useNavigate, useLocation } from "react-router-dom";
+import { environment } from "../Environment";
+import Header from "./components/Header";
 export default function Customerpage() {
-    let [someBooks, setsomeBooks] = React.useState([]);
-    let [Books, setBooks] = React.useState([]);
+	let [someBooks, setsomeBooks] = React.useState([]);
+	let Books = [];
 
-    Books.push( { id : 1 ,image_location:"../Images/pic4.png", 
-    name :"Book1",price:"20", description:"very good", available_amount:"15"});
-    Books.push( {id : 1 ,image_location:"../Images/pic4.png", 
-    name :"Book1",price:"20", description:"very good", available_amount:"15"});
-    Books.push( {id : 1 ,image_location:"../Images/pic4.png", 
-    name :"Book1",price:"20", description:"very good", available_amount:"15"});
-    Books.push( { id : 1 ,image_location:"../Images/pic4.png", 
-    name :"Book1",price:"20", description:"very good", available_amount:"15"});
-    Books.push( {id : 1 ,image_location:"../Images/pic4.png", 
-    name :"Book1",price:"20", description:"very good", available_amount:"15"});
-    Books.push( {id : 1 ,image_location:"../Images/pic4.png", 
-    name :"Book1",price:"20", description:"very good", available_amount:"15"});
-    Books.push( { id : 1 ,image_location:"../Images/pic4.png", 
-    name :"Book1",price:"20", description:"very good", available_amount:"15"});
-    Books.push( {id : 1 ,image_location:"../Images/pic4.png", 
-    name :"Book1",price:"20", description:"very good", available_amount:"15"});
-    Books.push( {id : 1 ,image_location:"../Images/pic4.png", 
-    name :"Book1",price:"20", description:"very good", available_amount:"15"});
-    const NAV = useNavigate();
-    const location = useLocation();
+	const NAV = useNavigate();
+	const location = useLocation();
 
-    //get some Books from the stock
-   /* React.useEffect(() => {
-        async function getBooks() {
-            let result = await fetch(`${environment.Host}/customer/getBooks`, {
-                method: "get",
-                headers: {
-                    'Content-type': 'application/json'
-                }
-            });
-            let res = await result.json();
-            setsomeBooks(res);
-        }
-        getBooks();
-    }, []);
-    
-        for(let i = 0 ; i < 20; i++){
-            let ordobj={name:someBooks[i].title,price:someBooks[i].price,id:someBooks[i].book_ISBN,image_location:"../Images/pic4.png"};
-            Books.push(ordobj);
-        }
+	//get some Books from the stock
+	React.useEffect(() => {
+		async function getBooks() {
+			let result = await fetch(`${environment.Host}/customer/getBooks`, {
+				method: "get",
+				headers: {
+					"Content-type": "application/json",
+				},
+			});
+			let res = await result.json();
+			setsomeBooks(res);
+		}
+		getBooks();
+	}, []);
 
-        function Logout(){
-            NAV('/Signin');
-         }
-         function Profile(){
-             //nav to profile
-             NAV('/Customerpage/Profile', {state:location.state.id});
-         }
-         let [cartid, setcartid]=React.useState();
-         let BookHtml ;
-        function start_shopping(){
-            setcartid(5);
-            async function start_shopping() {
-                let result = await fetch(`${environment.env}/getcartid`, {
-                    method: "POST",
-                    headers: {
-                        'Content-type': 'application/json'
-                    },
-                    body: JSON.stringify(
-                    {
-                      customerid:location.state.id
-                    }
-                  )
-                });
-                let res = await result.json();
-               cartid=  res;
-            }
-            start_shopping();
+	for (let i = 0; i < 20 && i < someBooks.length; i++) {
+		let ordobj = {
+			name: someBooks[i].title,
+			price: someBooks[i].price,
+			id: someBooks[i].book_ISBN,
+			image_location: "../Images/pic4.png",
+		};
+		Books.push(ordobj);
+	}
 
-        }
-        BookHtml = Books.map((item) => {
-            console.log(cartid);
-            return (
-                <BookCard
-                    img={item.image_location}
-                    name={item.name}
-                    price={item.price}
-                    id ={item.id}
-                    location={location}
-                    cartId = {cartid}
-                />
-            );
-        })
+	let BookHtml;
 
-    function Cart(){
-        //nav to Cart
-      //  NAV('/', {state:location.state.id});
-    }
-    return (
-        <div className="Main">
-            {header}
-            <div className="Explore">
-                <div className="container2">
-                    {BookHtml}
-                </div>
-                </div>
-            <div className='Cart' onClick={Cart}><BsFillCartCheckFill /></div>
+	BookHtml = Books.map((item) => {
+		return (
+			<BookCard
+				img={item.image_location}
+				name={item.name}
+				price={item.price}
+				id={item.id}
+				location={location}
+				cartId={location.state.username}
+			/>
+		);
+	});
 
-        </div>
-    );
+	function Cart() {
+		//nav to Cart
+		NAV("/", { state: location.state.id });
+	}
+	return (
+		<div className='Main'>
+			<Header location={location} cartid={location.state.username} />
+			<div className='Explore'>
+				<div className='container2'>{BookHtml}</div>
+			</div>
+			<div className='Cart' onClick={Cart}>
+				<BsFillCartCheckFill />
+			</div>
+		</div>
+	);
 }
--- Frontend/bookstore/src/CustomerPage/components/Header.js
+++ Frontend/bookstore/src/CustomerPage/components/Header.js
@@ -1,60 +1,61 @@
-import React from 'react';
-import {BsBoxArrowLeft , BsFillPersonFill, BsFillCartCheckFill, BsFillFilterCircleFill } from 'react-icons/bs';
-import './Header.css';
-import { useNavigate } from 'react-router-dom';
+import React from "react";
+import {
+	BsBoxArrowLeft,
+	BsFillPersonFill,
+	BsFillCartCheckFill,
+	BsFillFilterCircleFill,
+	BsSearch,
+} from "react-icons/bs";
+import "./Header.css";
+import { useNavigate } from "react-router-dom";
 
-export default function Header() {
-    let [search, setsearch]= React.useState("Title");
-    let cartid = 0 ;
-    function search_options(e){
-        setsearch(e.target.value);
-        console.log(search);
-    }
-    const NAV = useNavigate();
-    function Logout(){
-       NAV('/Signin');
-    }
-    function Profile(){
-        //nav to profile
-       // NAV('/', {state:location.state.id});
-    }
-    function Cart(){
-        //nav to Cart
-      // NAV('/', {state:location.state.id});
-    }
-    function start_shopping(){
-        async function start_shopping() {
-            let result = await fetch(`${environment.env}/getcartid`, {
-                method: "POST",
-                headers: {
-                    'Content-type': 'application/json'
-                },
-                body: JSON.stringify(
-                {
-                  customerid:location.state.id
-                }
-              )
-            });
-            let res = await result.json();
-            cartid = res;
-        }
-        start_shopping();
-    }
+export default function Header({ cartid }) {
+	let [search, setsearch] = React.useState("Title");
+	function search_options(e) {
+		setsearch(e.target.value);
+	}
+	const NAV = useNavigate();
 
-return(
-          
-          <div className='CHeader'>
-                <div onClick={Profile} className='Cart_2'>< BsFillPersonFill /></div>
-                <div  onClick={Cart} className='Cart_2'><BsFillCartCheckFill /></div>
-                <div  onClick={start_shopping} className='Cart_2'>< BsFillFilterCircleFill /></div>
-                <input className='search' placeholder={"search by "+search}/>
-                <select onChange={search_options}  className='select' name="Search" id="search">
-                <option value="Title">Title</option>
-                <option value="author">author</option>
-                <option value="category">category</option>
-                <option value="ISBN">ISBN</option>
-                <option value="publisher">publisher</option>
-                </select>
-                <div  onClick={Logout} className='Cart_1'><BsBoxArrowLeft /></div>
-          </div>
-   )}
+	function Profile() {
+		//nav to profile
+		NAV("/Customerpage/Profile", { state: cartid });
+	}
+	function Cart() {
+		//nav to Cart
+		NAV("/", { state: cartid });
+	}
+	function Logout() {
+		NAV("/Signin");
+	}
+	function goToSearch() {
+		NAV("/search", { state: cartid });
+	}
+	return (
+		<div className='CHeader'>
+			<div onClick={Profile} className='Cart_2'>
+				<BsFillPersonFill />
+			</div>
+			<div onClick={Cart} className='Cart_2'>
+				<BsFillCartCheckFill />
+			</div>
+			<div onClick={() => goToSearch()} className='Cart_2'>
+				<BsSearch />
+			</div>
+			<input className='search' placeholder={"search by " + search} />
+			<select
+				onChange={search_options}
+				className='select'
+				name='Search'
+				id='search'>
+				<option value='Title'>Title</option>
+				<option value='author'>author</option>
+				<option value='category'>category</option>
+				<option value='ISBN'>ISBN</option>
+				<option value='publisher'>publisher</option>
+			</select>
+			<div onClick={Logout} className='Cart_1'>
+				<BsBoxArrowLeft />
+			</div>
+		</div>
+	);
+}
